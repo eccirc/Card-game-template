9b56e7e4f499b70e8dc6616bd26bfac732bf107a
dagawi <dagawi@dagawi-pop.localdomain>
2022-02-01 14:22:38 +0000

    add test event listeners to main and discard pile elements

--- app.js
+++ app.js
@@ -28,19 +28,39 @@ class Player {
 }
 
 class Board {
-  constructor(remaining, discarded) {
-    this._remaining = remaining;
-    this._discarded = discarded;
+  constructor(main, discarded) {
+    this._mainPile = main;
+    this._discardPile = discarded;
+  }
+  get mainPile(){
+      return this._mainPile;
+  }
+  get discardPile(){
+      return this._discardPile;
+
   }
 }
 
 class Game {
   constructor(board) {
     this._board = board;
+    this._turn = 0;
     this._players = {
         player1 = new Player("Player 1"),
         player2 = new Player("Player 2")
     };
+  }
+  addMainPileListener(){
+      this._board.mainPile.addEventListener("click", (event) => {
+          console.log("main clicked")
+      })
+
+  }
+  addDiscardPileListner(){
+       this._board.discardPile.addEventListener("click", (event) => {
+          console.log("discarded clicked")
+      })
+
   }
 }
 
