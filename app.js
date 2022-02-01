1efc4df0f1037d6030e6e531cefc2492ebb82b1c
dagawi <dagawi@dagawi-pop.localdomain>
2022-02-01 14:38:35 +0000

    call click event methods in Game constructor to active them

--- app.js
+++ app.js
@@ -14,36 +14,12 @@ Events:
 
 */
 
-class Player {
-  constructor(name) {
-    this._name = name;
-    this._hand = [];
-  }
-  addCard(card) {
-    this._hand.push(card);
-  }
-  get hand() {
-    return this._hand;
-  }
-}
-
-class Board {
-  constructor(main, discarded) {
-    this._mainPile = main;
-    this._discardPile = discarded;
-  }
-  get mainPile() {
-    return this._mainPile;
-  }
-  get discardPile() {
-    return this._discardPile;
-  }
-}
-
 class Game {
   constructor(board) {
     this._board = board;
     this._turn = 0;
+    this.addMainPileListener();
+    this.addDiscardPileListner();
     this._players = {
       player1: new Player("Player 1"),
       player2: new Player("Player 2"),
@@ -58,6 +34,32 @@ class Game {
     this._board.discardPile.addEventListener("click", (event) => {
       console.log("discarded clicked");
     });
+  }
+}
+
+class Board {
+  constructor(mainPile, discardPile) {
+    this._mainPile = mainPile;
+    this._discardPile = discardPile;
+  }
+  get mainPile() {
+    return this._mainPile;
+  }
+  get discardPile() {
+    return this._discardPile;
+  }
+}
+
+class Player {
+  constructor(name) {
+    this._name = name;
+    this._hand = [];
+  }
+  addCard(card) {
+    this._hand.push(card);
+  }
+  get hand() {
+    return this._hand;
   }
 }
 
