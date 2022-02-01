ff3d3e379597e8d2eebec739784f851fbdac183c
dagawi <dagawi@dagawi-pop.localdomain>
2022-02-01 12:56:11 +0000

    Add instructions for basic first iteration and corresponding classes for Player, Board and Game

---
+++ app.js
@@ -0,0 +1,39 @@
+/*
+FIRST ITERATION - (VERY) BASIC FUNCTIONALITY
+ 
+Classes: 
+
+- Two players
+- One board
+- Very basic game logic
+
+Events:
+- player clicks on board 'deck' and adds one 'card' to their hand (Player class method ?)
+- then switch to the next player (Game class method ? )
+
+
+*/
+
+class Player {
+  constructor(name) {
+    this._name = name;
+    this._hand = [];
+  }
+}
+
+class Board {
+  constructor(remaining, discarded) {
+    this._remaining = remaining;
+    this._discarded = discarded;
+  }
+}
+
+class Game {
+  constructor(board) {
+    this._board = board;
+    this._players = {
+        player1 = new Player("Player 1"),
+        player2 = new Player("Player 2")
+    };
+  }
+}
